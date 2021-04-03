fcb40959f486fec7c63070b6317175c75356ca87
waloro99 <walterof1@hotmail.com>
2021-04-02 20:34:05 -0600

    add mongoose

--- BSBack/index.js
+++ BSBack/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const url = 'mongodb://localhost/ProductDBbs'
 const app = express();
 //const request =  require('request');
 //const async = require('async');
 const bodyParser = require('body-parser');
 const routes = require('./rutas/routes');
+
+mongoose.connect(url, {useNewUrlParser:true})
+const con = mongoose.connection
+
+con.on('open', () => {
+    console.log('connected...') 
+})
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
