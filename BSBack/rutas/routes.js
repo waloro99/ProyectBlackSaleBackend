70c8b7bccf884e8ef02f005bd5f26b3555ad2ab8
waloro99 <walterof1@hotmail.com>
2021-04-04 14:52:12 -0500

    Update verb post, get, put and delete

--- BSBack/rutas/routes.js
+++ BSBack/rutas/routes.js
@@ -7,7 +7,9 @@ let respuesta = {
     mensaje: ''
 };
 
-const express = require('express')
+const express = require('express');
+const { pseudoRandomBytes } = require('node:crypto');
+const product = require('../models/product');
 const router = express.Router()
 const Product = require('../models/product')
 
@@ -15,7 +17,7 @@ const Product = require('../models/product')
 const router = app => {
 
     //create
-    app.post('/api/v1/products', (req, res) => {
+    app.post('/api/v1/products', async(req, res) => {
         if(!req.body.name) {
             respuesta = {
             error: true,
@@ -23,7 +25,8 @@ const router = app => {
             mensaje: 'Todos los campos son requeridos'
             };
         } else {
-                producto = {
+            const product = new Product
+                ({
                     id: req.body.id,
                     name: req.body.name,
                     category: req.body.category,
@@ -31,19 +34,20 @@ const router = app => {
                     photo: req.body.photo,
                     stocks: req.body.stocks,
                     description: req.body.description
-                };
-                respuesta = {
+                })
+            respuesta = {
                 error: false,
                 codigo: 201,
                 mensaje: 'Producto creado',
-                respuesta: producto
-                };
+                respuesta: product
+            };
+            const c1 = await product.save()
         }
     res.send(respuesta);
     })  
 
     //read
-    app.get('/api/v1/products', (req, res) => {
+    app.get('/api/v1/products', async(req, res) => {
         respuesta = {
             error: false,
             codigo: 200,
@@ -59,9 +63,32 @@ const router = app => {
             respuesta = {
                 error: false,
                 codigo: 200,
-                mensaje: 'respuesta del producto',
-                respuesta: producto
+                mensaje: 'respuesta del producto',               
             };
+            const products = await Product.find()
+            }
+            res.send(respuesta);
+    })
+
+    app.get('/api/v1/products/:id', async(req, res) => {
+        respuesta = {
+            error: false,
+            codigo: 200,
+            mensaje: ''
+            };
+            if(producto === '') {
+            respuesta = {
+                error: true,
+                codigo: 501,
+                mensaje: 'El producto no ha sido creado'
+            };
+            } else {
+            respuesta = {
+                error: false,
+                codigo: 200,
+                mensaje: 'respuesta del producto',               
+            };
+            const products = await Product.findById(req.params.id)
             }
             res.send(respuesta);
     })
@@ -82,28 +109,41 @@ const router = app => {
                 mensaje: 'El producto no ha sido creado'
                 };
             } else {
-                producto = {
-                    id: req.body.id,
-                    name: req.body.name,
-                    category: req.body.category,
-                    price: req.body.price,
-                    photo: req.body.photo,
-                    stocks: req.body.stocks,
-                    description: req.body.description
-                };
+                const product = await Character.findById(req.params.id)
+                    if(req.body.id){
+                        product.id = req.body.id
+                    }
+                    if(req.body.name){
+                        product.name = req.body.name
+                    }
+                    if(req.body.category){
+                        product.category = req.body.category
+                    }
+                    if(req.body.price){
+                        product.price = req.body.price
+                    }
+                    if(req.body.photo){
+                        product.photo = req.body.photo
+                    }
+                    if(req.body.stocks){
+                        product.stocks = req.body.stocks
+                    }
+                    if(req.body.description){
+                        product.description = req.body.description
+                    }
                 respuesta = {
                 error: false,
                 codigo: 204,
-                mensaje: 'Producto actualizado',
-                respuesta: producto
+                mensaje: 'Producto actualizado'
                 };
+                const c1 = await product.save()
             }
         }
     res.send(respuesta);
     })
 
     //delete
-    app.delete('/api/v1/products', (req, res) => {
+    app.delete('/api/v1/products/:id', async(req, res) => {
         if(producto.name === '') {
             respuesta = {
             error: true,
@@ -116,28 +156,20 @@ const router = app => {
             codigo: 204,
             mensaje: 'Producto eliminado'
             };
-            producto = {
-                id: '',
-                name: '',
-                category: '',
-                price: '',
-                photo: '',
-                stocks: '',
-                description: ''
-            };
+            const product = await Product.findByIdAndRemove(req.params.id)
+            const c1 = await product.remove()
         }
     res.send(respuesta);
     })
 
-app.use(function(req, res, next) {
-    respuesta = {
-    error: true, 
-    codigo: 404, 
-    mensaje: 'URL no encontrada'
-    };
-    res.status(404).send(respuesta);
-});
-
+    app.use(function(req, res, next) {
+        respuesta = {
+        error: true, 
+        codigo: 404, 
+        mensaje: 'URL no encontrada'
+        };
+        res.status(404).send(respuesta);
+    });
 
 }
 // Export the router
