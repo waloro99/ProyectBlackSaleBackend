99994a37a7eb9eb46210dc7235b6c601834932b4
waloro99 <walterof1@hotmail.com>
2021-04-21 00:26:34 -0600

    add unit test mocha and chai and axios

---
+++ BSBack/test/productTest.spec.js
@@ -0,0 +1,12 @@
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+const products = require('./rutas/routes');
+const axios = require('axios');
+
+
+
+
+
+
